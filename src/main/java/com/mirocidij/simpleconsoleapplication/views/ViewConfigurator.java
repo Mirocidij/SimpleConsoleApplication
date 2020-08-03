453850cddaae7495fa57463b0dd283c3fff66b44
Yatsutko Sergey <s3rzh@inbox.ru>
2020-08-03 14:09:45 +0700

    refactor: move view configuration in outer class

---
+++ src/main/java/com/mirocidij/simpleconsoleapplication/views/ViewConfigurator.java
@@ -0,0 +1,31 @@
+package com.mirocidij.simpleconsoleapplication.views;
+
+import com.google.gson.Gson;
+import com.mirocidij.simpleconsoleapplication.controllers.AccountController;
+import com.mirocidij.simpleconsoleapplication.controllers.DeveloperController;
+import com.mirocidij.simpleconsoleapplication.controllers.SkillController;
+import com.mirocidij.simpleconsoleapplication.repositories.AccountRepository;
+import com.mirocidij.simpleconsoleapplication.repositories.DeveloperRepository;
+import com.mirocidij.simpleconsoleapplication.repositories.SkillRepository;
+import com.mirocidij.simpleconsoleapplication.views.general.IView;
+
+public class ViewConfigurator {
+    public static IView[] configure() {
+        Gson gson = new Gson();
+        // repo
+        SkillRepository skillRepository = new SkillRepository(gson, "skills.txt");
+        AccountRepository accountRepository = new AccountRepository(gson, "accounts.txt");
+        DeveloperRepository developerRepository =
+            new DeveloperRepository(gson, "developers.txt", skillRepository, accountRepository);
+        // controllers
+        SkillController skillController = new SkillController(skillRepository);
+        AccountController accountController = new AccountController(accountRepository);
+        DeveloperController developerController = new DeveloperController(developerRepository);
+        // views
+        SkillView skillView = new SkillView(skillController);
+        AccountView accountView = new AccountView(accountController);
+        DeveloperView developerView = new DeveloperView(developerController);
+
+        return new IView[]{ skillView, accountView, developerView };
+    }
+}
